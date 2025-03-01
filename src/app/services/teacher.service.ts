2ca14b1e0eed80d3006fbfe480064b9ac8098e4f
Shamaratun <shamaratunmeme@gmail.com>
2025-03-01 10:23:17 +0600

    teacher complete

--- src/app/services/teacher.service.ts
+++ src/app/services/teacher.service.ts
@@ -1,22 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Teacher} from '../teacher/teacher.component';
+import { Teacher } from '../teacher/teacher.component';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class TeacherService {
- 
-  apiUrl = "http://192.168.0.219:8080/teachers";
+  apiUrl = 'http://192.168.0.219:8080/teachers';
+
   constructor(private http: HttpClient) { }
 
   getTeachers(): Observable<any> {
     return this.http.get(this.apiUrl);
   }
- addTeacher(teacher: Teacher): Observable<Teacher> {
+
+  addTeacher(teacher: Teacher): Observable<Teacher> {
     return this.http.post<Teacher>(this.apiUrl, teacher);
   }
-  }
-  
-
+}
